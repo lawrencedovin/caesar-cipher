a5c06c43fd4dbafe1b4befa17d6832bea223f0dc
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-06 09:44:44 -0500

    Completed caesar cipher challenge

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,17 +1,15 @@
 letters = [chr(x) for x in range(ord('a'), ord('z') + 1)]
-print(letters[0])
 
-message = list(input('what is your message?: '))
+message = input('what is your message?: ')
 shift = int(input('how much do you want to shift the letters?: '))
 
-# def caesar_encode(message, shift):
-#     new_message = []
-#     for i, char in enumerate(message):
-#         for j, letter in enumerate(letters):
-#             if char[i] == letter[j]:
-#                 char[i] = letter[j+shift]
-#                 new_message.append(char[i])
-#     print(new_message)
+def caesar_encode(message, shift):
+    caesar_message = ''
+    for char in message:
+        if char == ' ':
+            caesar_message += char
+        if char in letters:
+            caesar_message += (letters[letters.index(char)+shift])
+    print(caesar_message)
 
-print(message)
 caesar_encode(message, shift)
