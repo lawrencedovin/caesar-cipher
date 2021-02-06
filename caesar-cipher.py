6d000d58a6324c2015051f65222a06978f72c6d0
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-06 10:07:44 -0500

    Updated caesar cipher to have decode

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,8 +1,5 @@
 letters = [chr(letter) for letter in range(ord('a'), ord('z') + 1)]
 
-message = input('what is your message?: ')
-shift = int(input('how much do you want to shift the letters?: '))
-
 def caesar_encode(message, shift):
     caesar_message = ''
     for char in message:
@@ -13,4 +10,21 @@ def caesar_encode(message, shift):
             caesar_message += char
     print(caesar_message)
 
-caesar_encode(message, shift)
+def caesar_decode(message, shift):
+    caesar_message = ''
+    for char in message:
+        if char in letters:
+            unshifted_letter = letters[letters.index(char) - shift]
+            caesar_message += unshifted_letter
+        else:
+            caesar_message += char
+    print(caesar_message)
+
+choice = input("Type 'encode' to encrypt, type 'decode' to decrypt > ").lower()
+while(choice != False):
+    if choice == 'encode' or choice == 'decode':
+        message = input('Type your message > ')
+        shift = int(input('Type the shift number > '))
+        caesar_encode(message, shift) if choice == 'encode' else caesar_decode(message, shift)
+    else:
+        choice = False
