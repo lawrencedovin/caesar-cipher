0d4d08ef05931fe2b7292f69ec077cbb56178762
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-06 10:23:55 -0500

    Refactored caesar cipher

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,30 +1,26 @@
 letters = [chr(letter) for letter in range(ord('a'), ord('z') + 1)]
+choice = True
 
-def caesar_encode(message, shift):
+def caesar_cipher(message, shift, choice):
     caesar_message = ''
     for char in message:
         if char in letters:
-            shifted_letter = letters[letters.index(char)+shift]
+            shifted_letter = letters[letters.index(char) + shift] if choice == 'encode' else letters[letters.index(char) - shift]
             caesar_message += shifted_letter
         else:
             caesar_message += char
     print(caesar_message)
 
-def caesar_decode(message, shift):
-    caesar_message = ''
-    for char in message:
-        if char in letters:
-            unshifted_letter = letters[letters.index(char) - shift]
-            caesar_message += unshifted_letter
-        else:
-            caesar_message += char
-    print(caesar_message)
-
-choice = input("Type 'encode' to encrypt, type 'decode' to decrypt > ").lower()
-while(choice != False):
+while(choice):
+    choice = input("Type 'encode' to encrypt, type 'decode' to decrypt > ").lower()
     if choice == 'encode' or choice == 'decode':
         message = input('Type your message > ')
         shift = int(input('Type the shift number > '))
-        caesar_encode(message, shift) if choice == 'encode' else caesar_decode(message, shift)
+        caesar_cipher(message, shift, choice)
+        try_again = input("Try again? Type 'yes' or 'no' > ").lower()
+        if try_again == 'yes':
+            continue
+        else: 
+            break  
     else:
-        choice = False
+        break
